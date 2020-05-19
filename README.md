98d045d275fa73ae8b78d4853f979f8c11bd2eb7
keddad <keddad@yandex.ru>
2020-05-19 18:16:20 +0300

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # paheal-parser
 Async file downloader for paheal r34
+
+Install deps form req.txt and off you go!
+
+```
+Usage: parser.py [OPTIONS] TAG # Tag to download images
+
+Options:
+  --pages-to-parse # How many pages to download, 10 by default
+  --download-videos / --no-download-videos # --download-videos by default
+  --skip-existing / --no-skip-existing # --skip-existing by default
+  --download-path # If not specified, downloads to ./TAG
+  --concurent-downloads # Maximum number of concurrent downloads, 10 by default
+  --vebrose / --no-vebrose # Log data currently downloaded in terminal
+  ```
---
+++ parser.py
@@ -0,0 +1,56 @@
+import typer
+import asyncio
+import aiohttp
+from pathlib import Path
+from click_spinner import spinner
+from utils import *
+
+BASE_LINK = "http://rule34.paheal.net/post/list/{}/{}"
+
+
+def main(tag: str, pages_to_parse: int = 10, download_videos: bool = True, skip_existing: bool = True, download_path: str = "", concurent_downloads: int = 10, vebrose: bool = False):
+    download_folder = Path(download_path or f"./{tag}")
+    download_folder.mkdir()
+    print("Getting number of available pages and checking if there are any")
+    with spinner():
+        actual_pages = get_pages_for_tag(BASE_LINK.format(tag, 1))
+
+    if actual_pages == 0:
+        print("There are no images on that tag")
+        return
+
+    if actual_pages < pages_to_parse:
+        print(
+            f"WARNING: There are only {actual_pages} available, but you requested {pages_to_parse}")
+    else:
+        actual_pages = pages_to_parse
+
+    print("Getting image links")
+
+    links_to_download = []
+
+    semaphore = asyncio.Semaphore(value=concurent_downloads)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(
+        asyncio.wait(
+            [get_elements_on_page(BASE_LINK.format(
+                tag, i), download_folder, links_to_download, semaphore) for i in range(1, actual_pages)]
+        )
+    )
+
+    print(f"Got {len(links_to_download)} objects to download, processing")
+
+    with spinner():
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(
+            asyncio.wait(
+                [download_file(t, download_folder, semaphore, vebrose, skip_existing) for t in links_to_download]
+            )
+        )
+
+    print("Done!")
+
+
+if __name__ == "__main__":
+    typer.run(main)
